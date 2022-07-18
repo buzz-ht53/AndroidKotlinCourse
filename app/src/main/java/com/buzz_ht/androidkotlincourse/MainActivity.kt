602355eb10921a22991028e116e78689fa0e6194
buzz-ht53 <akshayr.java@gmail.com>
2022-07-19 01:11:58 +0530

    debugging

--- app/src/main/java/com/buzz_ht/androidkotlincourse/MainActivity.kt
+++ app/src/main/java/com/buzz_ht/androidkotlincourse/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.buzz_ht.androidkotlincourse
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
+
+    }
+
+    fun logging(){
+
+        Log.d(TAG, "Hello World")
+        Log.wtf(TAG, "This will never happen")
+
     }
 }
